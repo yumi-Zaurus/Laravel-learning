56c009843af27b210b8fb5f2f78f8ae11c09e100
Yumi <k019c1409@g.neec.ac.jp>
2022-03-27 01:51:00 +0900

    お知らせAPIの作成'

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -9,4 +9,30 @@ class Notification extends Model
 {
     use SoftDeletes;
 
+    /**
+     * nortificationテーブル - importanceカラム
+     */
+    const IMPORTANCE_DEFAULT = 1; // デフォルト
+    const IMPORTANCE_MEDIUM = 2; // 重要度中
+    const IMPORTANCE_HIGH = 3; // 重要度高
+
+    /**
+     * 公開されているNotificationを取得する
+     * return array
+     */
+    static function getNotifications()
+    {
+        $notifications = Notification::where('is_open', 1)->get();
+        $notification_data = [];
+        foreach($notifications as $notification) {
+            $notification_data[] = [
+                "id" => $notification['id'],
+                "title" => $notification['title'],
+                "content" => $notification['contents'],
+                "date" => date('Y-m-d H:i', strtotime($notification['delivered_at'])),
+            ];
+        }
+        return $notification_data;
+    }
+
 }
